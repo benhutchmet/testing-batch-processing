06aca9b4c03780f170dd2ddc3086ea985dfcd76f
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-04-05 14:19:49 +0100

    working test script for selecting grid boxes for single year, single run, specific model (for one file). Might want to watch out for issues with the ri?p?f? section when there are multiple initializations or p or f

--- multi-model/testing-multi-model-sel-gridbox.bash
+++ multi-model/testing-multi-model-sel-gridbox.bash
@@ -2,8 +2,8 @@
 
 # testing-multi-model-sel-gridbox.bash
 #
-# Usage:  testing-multi-model-sel-gridbox.bash <year> <run> <location> <model>
-#
+# Usage:  testing-multi-model-sel-gridbox.bash <year> <run> <location> <model_group> <model>
+# For example: testing-multi-model-sel-gridbox.bash 1960 1 azores CMCC CMCC-CM2-SR5
 
 # set the partition/account arguments for LOTUS based on usage context
 # SBATCH --partition=short-serial
@@ -12,13 +12,16 @@
 year=$1
 run=$2
 
+# extract the location from the command line
+location=$3
+
 # set up an if loop for the location gridbox selection
-if [ $3 == "azores" ]; then
+if [ $location == "azores" ]; then
     lon1=-28
     lon2=-20
     lat1=36
     lat2=40
-elif [ $3 == "iceland" ]; then
+elif [ $location == "iceland" ]; then
     lon1=-25
     lon2=-16
     lat1=63
@@ -34,9 +37,23 @@ model=$5
 
 # set up the output directory
 # in the scratch-nopw directory
-OUTPUT_DIR=/work/scratch-nopw/benhutch/$model/$3/outputs
+OUTPUT_DIR=/work/scratch-nopw/benhutch/$model/$location/outputs
 # make the output directory if it doesn't exist
 mkdir -p $OUTPUT_DIR
 
 # set the files to be processed
-files=/badc/cmip6/data/CMIP6/DCPP/$model_group/$model/dcppA-hindcast/s${year}-r${run}i1p1f1/Amon/psl/gn/files/d????????/*.nc
+# might run into issues with the ? in the path i?p?f?
+files=/badc/cmip6/data/CMIP6/DCPP/$model_group/$model/dcppA-hindcast/s${year}-r${run}i?p?f?/Amon/psl/gn/files/d????????/*.nc
+
+# activate the environment containing CDO
+module load jaspy
+
+# loop through the files and process them
+for INPUT_FILE in $files; do
+
+    echo "[INFO] Subsetting: $INPUT_FILE"
+    fname=${location}-$(basename $INPUT_FILE)
+    OUTPUT_FILE=$OUTPUT_DIR/$fname
+    cdo sellonlatbox,$lon1,$lon2,$lat1,$lat2 $INPUT_FILE $OUTPUT_FILE
+
+done
