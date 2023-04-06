1308ad1b439e08c58134eb9ad15f118549f4fe26
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-04-06 11:42:07 +0100

    initial commit of testing files for combining anomalies across multiple models

--- multi-model/process-scripts/multi-model.combine_anom.bash
+++ multi-model/process-scripts/multi-model.combine_anom.bash
@@ -1,34 +1,39 @@
 #!/bin/bash
 
-# combine_anom_test.bash
+# multi-model.combine_anom.bash
 #
-# Usage: combine_anom_test.bash <location> <year>
+# For example: multi-model.combine_anom.bash azores BCC-CSM2-MR 1961
 #
 
-# set the location
+USAGE_MESSAGE="Usage: multi-model.combine_anom.bash <location> <model> <year>"
+
+# check that the correct number of arguments have been passed
+if [ $# -ne 3 ]; then
+    echo "$USAGE_MESSAGE"
+    exit 1
+fi
+
+# extract the location, model and year
 location=$1
-
-# set the year
-year=$2
+model=$2
+year=$3
 
 # set the output directory
-OUTPUT_DIR=/home/users/benhutch/BCC-CSM2-MR/$location/combine-anom-test
-
+OUTPUT_DIR=/work/scratch-nopw/benhutch/$model/$location/outputs/ensemble-mean
 # make the output directory if it doesn't exist
 mkdir -p $OUTPUT_DIR
 
 # set the files to be processed
-files=/home/users/benhutch/BCC-CSM2-MR/$location/ensmean-year-2-9-anomaly/subtracted-model-mean-years-2-9-DJFM-${location}-psl_Amon_BCC-CSM2-MR_dcppA-hindcast_s${year}*.nc
-
-# echo the files to be processed
-echo "[INFO] files to be processed: $files"
+files="/work/scratch-nopw/benhutch/$model/$location/outputs/subtracted-MM-state-*_s${year}*.nc"
 
 # activate the environment containing CDO
 module load jaspy
 
-# set up the ensemble mean name
-fname_ensmean=ensmean-year-2-9-anomaly-${year}.nc
-OUTPUT_FILE_ENSMEAN=$OUTPUT_DIR/$fname_ensmean
+# set up the ensemble mean file name
+ensemble_mean_fname="ensemble-mean-years-2-9-DJFM-anomalies-${location}-psl_Amon_${model}_dcppA-hindcast_s${year}.nc"
+
+# set up the ensemble mean file path
+ensemble_mean_file="$OUTPUT_DIR/$ensemble_mean_fname"
 
 # calculate the ensemble mean
-cdo ensmean $files $OUTPUT_FILE_ENSMEAN
+cdo ensmean $files $ensemble_mean_file
--- multi-model/submit-scripts/submit-all-mulit-model.combine_anom.bash
+++ multi-model/submit-scripts/submit-all-mulit-model.combine_anom.bash
@@ -1,34 +1,79 @@
 #!/bin/bash
 
-# submit-all-BCC-CSM2-MR.combine_anom.bash
+# submit-all-multi-model.combine_anom.bash
 #
-# Usage:   submit-all-BCC-CSM2-MR.combine_anom.bash <location> <start_year> <finish_year>
+# For example: submit-all-multi-model.combine_anom.bash azores BCC-CSM2-MR 1961 1969
+#
 
-# set the partition/account arguments for LOTUS based on usage context
-# SBATCH --partition=short-serial
+USAGE_MESSAGE="Usage: submit-all-multi-model.combine_anom.bash <location> <model> <start_year> <finish_year>"
 
-# extract the location
+# check that the correct number of arguments have been passed
+if [ $# -ne 4 ]; then
+    echo "$USAGE_MESSAGE"
+    exit 1
+fi
+
+# extract the location, model, start year and finish year
 location=$1
+model=$2
+start_year=$3
+finish_year=$4
 
-# extract the start and finish years
-start_year=$2
-finish_year=$3
+# set the extractor script
+EXTRACTOR=$PWD/multi-model.combine_anom.bash
 
-# set the extractor script and the output directory
-EXTRACTOR=$PWD/BCC-CSM2-MR.combine_anom.bash
-OUTPUTS_DIR=/home/users/benhutch/BCC-CSM2-MR/$location/lotus-outputs
+# set up the models list
+models="BCC-CSM2-MR MPI-ESM1-2-HR CanESM5 CMCC-CM2-SR5"
 
-# make the output directory if it doesn't exist
-mkdir -p $OUTPUTS_DIR
+# loop through the models
+if [ $model == "all" ]; then
 
-# create an outer loop to loop through the years
-for year in $(seq $start_year $finish_year); do
+    for model in $models; do
 
-    # set the date
-    year=$(printf "%d" $year)
-    echo "[INFO] Submitting job to LOTUS for year: $year"
-    # Submit the job to LOTUS
-    sbatch --partition=short-serial -t 5 -o $OUTPUTS_DIR/${year}.%j.out \
-           -e $OUTPUTS_DIR/${year}.%j.err $EXTRACTOR $location $year
+        # echo the model name
+        echo "[INFO] Combining anomalies for model: $model"
 
-done
+        # set the output directory
+        OUTPUTS_DIR=/work/scratch-nopw/benhutch/$model/$location/lotus-outputs
+
+        # make the output directory if it doesn't exist
+        mkdir -p $OUTPUTS_DIR
+
+        # create an outer loop to loop through the years
+        for year in $(seq $start_year $finish_year); do
+
+            # set the date
+            year=$(printf "%d" $year)
+            echo "[INFO] Submitting job to LOTUS for year: $year"
+            # Submit the job to LOTUS
+            sbatch --partition=short-serial -t 5 -o $OUTPUTS_DIR/${year}.%j.out \
+                   -e $OUTPUTS_DIR/${year}.%j.err $EXTRACTOR $location $model $year
+
+        done
+
+    done
+
+else
+
+    # echo the model name
+    echo "[INFO] Combining anomalies for model: $model"
+
+    # set the output directory
+    OUTPUTS_DIR=/work/scratch-nopw/benhutch/$model/$location/lotus-outputs
+
+    # make the output directory if it doesn't exist
+    mkdir -p $OUTPUTS_DIR
+
+    # create an outer loop to loop through the years
+    for year in $(seq $start_year $finish_year); do
+
+        # set the date
+        year=$(printf "%d" $year)
+        echo "[INFO] Submitting job to LOTUS for year: $year"
+        # Submit the job to LOTUS
+        sbatch --partition=short-serial -t 5 -o $OUTPUTS_DIR/${year}.%j.out \
+               -e $OUTPUTS_DIR/${year}.%j.err $EXTRACTOR $location $model $year
+
+    done
+
+fi
