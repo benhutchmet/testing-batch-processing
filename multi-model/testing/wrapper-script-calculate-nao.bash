af66e5e23380ae1743d93ccc028c0bd79aaca64b
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-04-06 16:40:17 +0100

    wrapper script updated - but struggles with mergetime for some reason

--- multi-model/testing/wrapper-script-calculate-nao.bash
+++ multi-model/testing/wrapper-script-calculate-nao.bash
@@ -22,8 +22,6 @@ model=$1
 start_year=$2
 finish_year=$3
 
-# extract the scripts to be run
-scripts-directory=/home/users/benhutch/multi-model/
 
 # process scripts
 # don't think these are needed
@@ -37,46 +35,46 @@ scripts-directory=/home/users/benhutch/multi-model/
 # calc-nao=${scripts-directory}/multi-model.NAO-anom.bash
 
 # submit scripts
-submit-select-gridbox=${scripts-directory}/submit-all-multi-model.bash
-submit-select-2-9-DJFM=${scripts-directory}/submit-all-multi-model.year-2-9-DJFM.bash
-submit-calc-MM-state=${scripts-directory}/submit-all-multi-model.calc-bl.bash
-submit-calc-anomalies=${scripts-directory}/submit-all-multi-model.sub_MM.bash
-submit-combine-anomalies=${scripts-directory}/submit-all-multi-model.combine_anom.bash
-submit-time-mean=${scripts-directory}/submit-all-multi-model.time-mean.bash
-submit-mergetime=${scripts-directory}/submit-all-multi-model.mergetime.bash
-submit-calc-nao=${scripts-directory}/submit-all-multi-model.NAO-anom.bash
+submit_select_gridbox=$PWD/submit-all-multi-model.bash
+submit_select_2_9_DJFM=$PWD/submit-all-multi-model.year-2-9-DJFM.bash
+submit_calc_MM_state=$PWD/submit-all-multi-model.calc-bl.bash
+submit_calc_anomalies=$PWD/submit-all-multi-model.sub_MM.bash
+submit_combine_anomalies=$PWD/submit-all-multi-model.combine_anom.bash
+submit_time_mean=$PWD/submit-all-multi-model.time-mean.bash
+submit_mergetime=$PWD/submit-all-multi-model.mergetime.bash
+submit_calc_nao=$PWD/submit-all-multi-model.NAO-anom.bash
 
 # call the scripts for both iceland and azores
 # select the gridboxes
-bash ${submit-select-gridbox azores} $start_year $finish_year $model
-bash ${submit-select-gridbox iceland} $start_year $finish_year $model
+bash ${submit_select_gridbox} azores $start_year $finish_year $model
+bash ${submit_select_gridbox} iceland $start_year $finish_year $model
 
-# select the years 2-9 and DJFM
-bash ${submit-select-2-9-DJFM} azores $model
-bash ${submit-select-2-9-DJFM} iceland $model
+# select the years 2_9 and DJFM
+bash ${submit_select_2_9_DJFM} azores $model
+bash ${submit_select_2_9_DJFM} iceland $model
 
-# calculate the multi-model state
-bash ${submit-calc-MM-state} azores $model
-bash ${submit-calc-MM-state} iceland $model
+# calculate the multi_model state
+bash ${submit_calc_MM_state} azores $model
+bash ${submit_calc_MM_state} iceland $model
 
 # calculate the anomalies
-bash ${submit-calc-anomalies} azores $model $start_year $finish_year
-bash ${submit-calc-anomalies} iceland $model $start_year $finish_year
+bash ${submit_calc_anomalies} azores $model $start_year $finish_year
+bash ${submit_calc_anomalies} iceland $model $start_year $finish_year
 
 # combine the anomalies
-bash ${submit-combine-anomalies} azores $model $start_year $finish_year
-bash ${submit-combine-anomalies} iceland $model $start_year $finish_year
+bash ${submit_combine_anomalies} azores $model $start_year $finish_year
+bash ${submit_combine_anomalies} iceland $model $start_year $finish_year
 
 # time mean the anomalies
-bash ${submit-time-mean} azores $model $start_year $finish_year
-bash ${submit-time-mean} iceland $model $start_year $finish_year
+bash ${submit_time_mean} azores $model $start_year $finish_year
+bash ${submit_time_mean} iceland $model $start_year $finish_year
 
 # merge the time
-bash ${submit-mergetime} azores $model
-bash ${submit-mergetime} iceland $model
+bash ${submit_mergetime} azores $model
+bash ${submit_mergetime} iceland $model
 
 # calculate the NAO anomaly
-bash ${submit-calc-nao} $model
+bash ${submit_calc_nao} $model
 
 
 
