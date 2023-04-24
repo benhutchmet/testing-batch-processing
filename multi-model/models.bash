fc781c904b62318006f1ca7e8216f7ccdc994b6f
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-04-24 10:08:14 +0100

    updated the models file to include the models to be downloaded and also scripted a new file to generate the wget scripts for downloading from the different models - not yet tested.

--- multi-model/models.bash
+++ multi-model/models.bash
@@ -4,8 +4,14 @@
 # file containing the list of models to be processed
 
 # list of models
+# available on JASMIN
 models="BCC-CSM2-MR MPI-ESM1-2-HR CanESM5 CMCC-CM2-SR5 HadGEM3-GC31-MM EC-Earth3"
 
 single_file_models="BCC-CSM2-MR MPI-ESM1-2-HR CanESM5 CMCC-CM2-SR5"
 
 multi_file_models="HadGEM3-GC31-MM EC-Earth3"
+
+# models downloaded from CEDA/LLNL ESGF server to xfer
+CEDA_models="CESM1-1-CAM5-CMIP5 IPSL-CM6A-LR MIROC6 CNRM-ESM2-1 FGOALS-f3-L MPI-ESM1-2-LR MRI-ESM2-0"
+
+ESGF_models="NorCPM1 EC-Earth3-HR"
