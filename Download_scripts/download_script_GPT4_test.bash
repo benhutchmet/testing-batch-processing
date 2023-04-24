d67e25b49e3b2fca5adfbc6aa7b172f09aeb0b37
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-04-24 10:34:13 +0100

    models list and script updated to account for different node specifications

--- Download_scripts/download_script_GPT4_test.bash
+++ Download_scripts/download_script_GPT4_test.bash
@@ -9,8 +9,10 @@
 # remember to reupload this!
 source /home/users/benhutch/multi-model/models.bash
 # echo the lists to be used
-echo "models to be downloaded from CEDA: $CEDA_models"
-echo "models to be downloaded from ESGF: $ESGF_models"
+echo  "[INFO] Models to be downloaded: $ESGF_models"
+echo  "[INFO] Nodes to be downloaded from: $ESGF_nodes"
+
+# models and nodes correspond
 
 # set the constants for the url to be used
 # for the CEDA models
@@ -18,46 +20,40 @@ project="CMIP6"
 experiment_id="dcppA-hindcast"
 table_id="Amon"
 variable_id="psl"
-limit=8000
-data_node="esgf.ceda.ac.uk"
+limit=10000
+
+# echo the characteristics of the data to be downloaded
+echo "[INFO] Project: $project"
+echo "[INFO] Experiment: $experiment_id"
+echo "[INFO] Table: $table_id"
+echo "[INFO] Variable: $variable_id"
+echo "[INFO] Limit: $limit"
 
 # set the directory to download wget scripts to
 wget_scripts_dir="/home/users/benhutch/multi-model/download_scripts/wget_scripts/"
 
-# loop over the CEDA models
-for model in $CEDA_models ; do
-    
-    echo "For the CEDA models: $CEDA_models"
-    echo "Downloading wget script for $model"
+# loop over the models and nodes
+# and download the wget scripts
+for ((i=0; i<${#ESGF_models[@]}; i++)); do
 
-    # set the source_id
-    source_id=$model
-    # construct the url
-    url="https://esgf-data.dkrz.de/esg-search/wget?project=$project&experiment_id=$experiment_id&source_id=$source_id&table_id=$table_id&variable_id=$variable_id&limit=$limit&data_node=$data_node"
+    # set the model and node
+    model=${ESGF_models[$i]}
+    node=${ESGF_nodes[$i]}
+
+    # echo the model and node
+    echo "[INFO] Downloading wget script for $model from $node"
+
+    # set the url
+    url="https://esgf-data.dkrz.de/esg-search/wget?project=$project&experiment_id=$experiment_id&source_id=$model&table_id=$table_id&variable_id=$variable_id&limit=$limit&data_node=$node"
+    
     # set the wget script name
-    wget_script="${wget_scripts_dir}${model}_wget_script.sh"
+    wget_script_name="wget_script_$model.bash"
+    # set the wget script path
+    wget_script_path="$wget_scripts_dir$wget_script_name"
+    
     # download the wget script
-    wget -O $wget_script $url
-    # make the script executable
-    chmod +x $wget_script
-    
-done
+    wget -0 $wget_script_path $url
+    # make the wget script executable
+    chmod +x $wget_script_path
 
-# then loop over the ESGF models
-for model in $ESGF_models ; do
-    
-    echo "For the ESGF models: $ESGF_models"
-    echo "Downloading wget script for $model"
-
-    # set the source_id
-    source_id=$model
-    # construct the url
-    url="https://esgf-data.dkrz.de/esg-search/wget?project=$project&experiment_id=$experiment_id&source_id=$source_id&table_id=$table_id&variable_id=$variable_id&limit=$limit&data_node=$data_node"
-    # set the wget script name
-    wget_script="${wget_scripts_dir}${model}_wget_script.sh"
-    # download the wget script
-    wget -O $wget_script $url
-    # make the script executable
-    chmod +x $wget_script
-    
 done
--- multi-model/models.bash
+++ multi-model/models.bash
@@ -12,6 +12,28 @@ single_file_models="BCC-CSM2-MR MPI-ESM1-2-HR CanESM5 CMCC-CM2-SR5"
 multi_file_models="HadGEM3-GC31-MM EC-Earth3"
 
 # models downloaded from CEDA/LLNL ESGF server to xfer
-CEDA_models="CESM1-1-CAM5-CMIP5 IPSL-CM6A-LR MIROC6 CNRM-ESM2-1 FGOALS-f3-L MPI-ESM1-2-LR MRI-ESM2-0"
+# NorCPM1
+# EC-Earth3-HR
+# CESM1-1-CAM5-CMIP5
+# IPSL-CM6A-LR
+# MIROC6
+# CNRM-ESM2-1
+# FGOALS-fs-L
+# MPI-ESM1-2-LR
+# MRI-ESM2-0
 
-ESGF_models="NorCPM1 EC-Earth3-HR"
+# corresponding nodes
+# noresg.nird.sigma2.no
+# esg-dn1.nsc.liu.se
+# esgf-data.ucar.edu
+# vesg.ipsl.upmc.fr
+# esgf-data02.diasjp.net
+# esg1.umr-cnrm.fr
+# esg.lasg.ac.cn
+# esgf.dwd.de
+# esgf-data03.diasjp.net 
+
+ESGF_models="NorCPM1 EC-Earth3-HR CESM1-1-CAM5-CMIP5 IPSL-CM6A-LR MIROC6 CNRM-ESM2-1 FGOALS-fs-L MPI-ESM1-2-LR MRI-ESM2-0"
+
+# corresponding nodes
+ESGF_nodes="noresg.nird.sigma2.no esg-dn1.nsc.liu.se esgf-data.ucar.edu vesg.ipsl.upmc.fr esgf-data02.diasjp.net esg1.umr-cnrm.fr esg.lasg.ac.cn esgf.dwd.de esgf-data03.diasjp.net"
