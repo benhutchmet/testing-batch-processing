9740ba7c1fae58bf44263b45bb3b69af5996b616
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-05-12 15:52:52 +0100

    script seems to work for downloading all of the sfcWind, tas and rsds files. These will need to be moved into their own folders though.

--- Download_scripts/run_wget_scripts.bash
+++ Download_scripts/run_wget_scripts.bash
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Usage: bash run_wget_scripts.bash <variable>
+
+# load the models.bash file
+source /home/users/benhutch/multi-model/models.bash
+
+# exctract the variable from the command line
+var=$1
+
 # Define the directory containing the wget scripts
 # make sure that these are the modified wget scripts
-WGET_SCRIPTS_DIR="/home/users/benhutch/multi-model/download_scripts/wget_scripts_mod"
+WGET_SCRIPTS_DIR="/home/users/benhutch/multi-model/download_scripts/wget_scripts"
 
 # Function to display elapsed time in a human-readable format
 function display_duration() {
@@ -18,17 +26,27 @@ function display_duration() {
     printf '%d seconds\n' $S
 }
 
+# # if else statement to determine which models to run
+# if [ "$var" == "sfcWind" ]; then
+#     # set the models to run
+#     models=$wind_speed_ESGF_models
+# elif [ "$var" == "tas" ]; then
+#     # set the models to run
+#     models=$tas_ESGF_models
+# elif [ "$var" == "rsds" ]; then
+#     # set the models to run
+#     models=$rsds_ESGF_models
+# else
+#     echo "Variable not recognised. Please choose from: sfcWind, tas"
+#     exit 1
+# fi
+
+
 # Execute each wget script and print progress with elapsed time
 # CNRM not working for now
 start_time=$(date +%s)
-for script in "${WGET_SCRIPTS_DIR}/wget_script_EC-Earth3-HR.bash" \
-              "${WGET_SCRIPTS_DIR}/wget_script_FGOALS-f3-L.bash" \
-              "${WGET_SCRIPTS_DIR}/wget_script_IPSL-CM6A-LR.bash" \
-              "${WGET_SCRIPTS_DIR}/wget_script_MIROC6.bash" \
-              "${WGET_SCRIPTS_DIR}/wget_script_MPI-ESM1-2-LR.bash" \
-              "${WGET_SCRIPTS_DIR}/wget_script_MRI-ESM2-0.bash" \
-              "${WGET_SCRIPTS_DIR}/wget_script_NorCPM1.bash"
-do
+for script in "$WGET_SCRIPTS_DIR"/wget_script_*_"$var".bash; do
+    
     model_name=$(basename "$script" | sed 's/^wget_script_//' | sed 's/\.bash$//')
     echo "Downloading $model_name data..."
     script_start_time=$(date +%s)
@@ -40,5 +58,5 @@ do
     current_time=$(date +%s)
     total_duration=$((current_time - start_time))
     echo "Current runtime: $(display_duration $total_duration)"
-    echo
+    echo " "
 done
